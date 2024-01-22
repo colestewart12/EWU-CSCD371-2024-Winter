e333e8f88ee786ad6bed99e22f2ea3f5b8c896c6
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-22 14:42:05 -0800

    LogFactory implemented and tested

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+    [TestMethod]
+    //MethodUnderTest_ConditionUnderTest_ExpectedResult
+    public void Log_OutputEntryToLog_SuccesfulOutput()
+    {
+        string filePath = "testFile.txt";
+        string message = "Hello from file";
+
+        FileLogger filelog = new FileLogger(filePath);
+
+        filelog.Log(LogLevel.Warning, message);
+
+        Assert.IsTrue(File.ReadAllText(filePath).Contains($" FileLogger Warning: {message}"));
+        File.Delete(filePath);
+
+    }
 
 }
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,8 +2,24 @@
 
 public class LogFactory
 {
-    public BaseLogger CreateLogger(string className)
+
+    private string? fileLoggerPath;
+
+    public void ConfigureFileLogger(string filePath)
     {
+        fileLoggerPath = filePath;
+    }
+
+    public BaseLogger? CreateLogger()
+    {
+        if (fileLoggerPath != null)
+        {
+            BaseLogger baseLogger = new FileLogger(fileLoggerPath)
+            {
+                ClassName = nameof(LogFactory)
+            };
+            return baseLogger;
+        }
 
         return null;
     }
