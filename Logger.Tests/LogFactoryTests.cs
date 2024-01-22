1f07cc9cf662676d25038bff419497132ef26c1f
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-22 14:41:14 -0800

    FileLogger created and tested

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -5,5 +5,21 @@ namespace Logger.Tests;
 [TestClass]
 public class LogFactoryTests
 {
+    [TestMethod]
+    //MethodUnderTest_ConditionUnderTest_ExpectedResult
+    public void CreateLogger_ValidPathString_LoggerCreatedAndReturned()
+    {
+        LogFactory factory = new LogFactory();
+        factory.ConfigureFileLogger("testpath.txt");
+        Assert.IsNotNull(factory.CreateLogger());
+    }
+
+    [TestMethod]
+    public void CreateLogger_NoPathString_NullReturned()
+    {
+        LogFactory factory = new LogFactory();
+        Assert.IsNull(factory.CreateLogger());
+    }
 
 }
+
--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -1,7 +1,33 @@
-﻿namespace Logger;
+﻿using System;
+using System.IO;
+
+
+namespace Logger;
 
 public abstract class BaseLogger
 {
+
+    public string? ClassName { get; set; }
     public abstract void Log(LogLevel logLevel, string message);
 }
 
+public class FileLogger : BaseLogger
+{
+    private readonly string _fileName;
+
+    public FileLogger(string fileName)
+    {
+        _fileName = fileName;
+    }
+
+    public override void Log(LogLevel logLevel, string message)
+    {
+        string dateTime = DateTime.Now.ToString("M/d/yyyy hh:mm:ss tt");
+
+        string className = nameof(FileLogger);
+
+        string logOutput = $"{dateTime} {className} {logLevel}: {message}";
+
+        File.AppendAllText(_fileName, logOutput);
+    }
+}
