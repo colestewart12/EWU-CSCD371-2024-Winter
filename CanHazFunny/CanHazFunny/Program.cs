5717b3e81dbdb192c1f94aba054b8e3d5c53adfa
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-26 23:28:47 -0800

    Program implemented to write to Console

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,8 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //Feel free to use your own setup here - this is just provided as an example
-            //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
 
             var OutputInstance = new JokeOutput();
             var ServiceInstance = new JokeService();
