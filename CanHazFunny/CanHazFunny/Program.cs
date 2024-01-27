b93d37e14c35902319f3c164302e51cb1cfef7f0
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-27 08:32:35 -0800

    Update access modification levels for Program class

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace CanHazFunny;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
 
         var OutputInstance = new JokeOutput();
