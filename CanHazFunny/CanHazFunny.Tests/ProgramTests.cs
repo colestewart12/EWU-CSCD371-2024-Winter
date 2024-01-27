078a442d59589febed2c775fbf77c1b4e95eb328
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-26 23:27:15 -0800

    Program unit tests setup

---
+++ CanHazFunny/CanHazFunny.Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace CanHazFunny.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void WriteJoke_WritesToConsole_SuccessfulReturn()
+        {
+            //Arrange
+            var consoleOutput = new StringWriter();
+
+            //Redirect Console to StringWriter
+            Console.SetOut(consoleOutput);
+            var testOutput = new JokeOutput();
+            string joke = "Test joke!";
+
+            //Act
+            testOutput.WriteJoke(joke);
+
+            //Assert
+            string consoleText = consoleOutput.ToString().Trim();
+            Assert.AreEqual(joke, consoleText);
+
+        }
+    }
+}
