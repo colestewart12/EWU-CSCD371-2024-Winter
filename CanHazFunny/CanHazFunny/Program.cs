f28eec62acc0ce28df7e522d33ca4ad8162dddc0
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-26 23:27:47 -0800

    Reapply "Program Class created"
    This reverts commit 53d74698a30b7a2244f3da4e6c7078557cb7619c.

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -6,6 +6,11 @@
         {
             //Feel free to use your own setup here - this is just provided as an example
             //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
+
+            var OutputInstance = new JokeOutput();
+            var ServiceInstance = new JokeService();
+
+            new Jester(OutputInstance, ServiceInstance).TellJoke();
         }
     }
 }
