829c89fca499c31a488230906451dff8f45783ca
Cole Stewart <colesoccerstewy@gmail.com>
2024-01-26 22:32:05 -0800

    Jester class functioning and unit tests are passing

---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+
+namespace CanHazFunny
+{
+    public class Jester(IJokeOutput jokeOutput, IJokeService jokeService)
+    {
+        private readonly IJokeService _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+        private readonly IJokeOutput _jokeOutput = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
+
+        public void TellJoke()
+        {
+            string joke;
+            do
+            {
+                joke = _jokeService.GetJoke();
+            }while (joke.Contains("Chuck Norris"));
+
+            _jokeOutput.WriteJoke(joke);
+        }
+    }
+}
