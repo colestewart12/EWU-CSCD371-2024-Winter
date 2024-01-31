9a05332e0b73ca7399959c0109e143aa64575d77
colestewart12 <120848582+colestewart12@users.noreply.github.com>
2024-01-31 13:29:15 -0800

    Update CanHazFunny/CanHazFunny/Program.cs

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -5,9 +5,6 @@ public  sealed class Program
     public static void Main(string[] args)
     {
 
-        var OutputInstance = new JokeOutput();
-        var ServiceInstance = new JokeService();
-new Jester(new JokeOutput(), new JokeService()).TellJoke();
-        new Jester(OutputInstance, ServiceInstance).TellJoke();
+        new Jester(new JokeOutput(), new JokeService()).TellJoke();
     }
 }
