912e40ce042454f23adeffa2470f11c28c4d1a1a
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-03 10:21:10 -0800

    FullName implemented and initial tests created

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Logger.Tests;
+
+public class FullNameTests
+{
+    [Fact]
+    public void FullName_RecordCreated_SuccessfulCreation()
+    {
+        //Arrange
+        var FullName = new FullName("First", "Middle", "Last");
+
+        //Act and Assert
+        Assert.Equal("First", FullName.FirstName);
+        Assert.Equal("Middle", FullName.MiddleName);
+        Assert.Equal("Last", FullName.LastName);
+
+    }
+
+    [Fact]
+    public void FullName_NullValues_SuccessfulReturn()
+    {
+        //Arrange
+        var FullName = new FullName(null, null, null);
+
+        //Act and Assert
+        Assert.Equal("", FullName.FirstName);
+        Assert.Equal("", FullName.MiddleName);
+        Assert.Equal("", FullName.LastName);
+    }
+}
---
+++ Logger/FullName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Logger;
+
+public record FullName
+{
+    public string? FirstName { get; }
+    public string? MiddleName { get; }
+    public string? LastName { get; }
+
+    public FullName(string? firstName, string? middleName, string? lastName)
+    {
+        FirstName = firstName ?? "";
+        MiddleName = middleName ?? "";
+        LastName = lastName ?? "";
+    }
+}
