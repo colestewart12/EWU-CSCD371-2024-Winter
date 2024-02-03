d8896278dfb783e36697e6a22812f3bbe4ad0775
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-03 15:35:03 -0800

    BaseEntity implemented and tested, modifications to FullName

---
+++ Logger.Tests/BaseEntityTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class BaseEntityTests
+{
+    private class TestEntity : BaseEntity
+    {
+        private string _name;
+
+        public TestEntity(Guid id, string name)
+        {
+            _name = name;
+            Id = id;
+        }
+
+        protected override string GetName()
+        {
+            return _name;
+        }
+
+        protected override void SetName(string name)
+        {
+            _name = name;
+        }
+    }
+
+    [Fact]
+    public void BaseEntity_SetId_SuccessfulSet()
+    {
+        //Arrange
+        var entityId = Guid.NewGuid();
+        var testEntity = new TestEntity(entityId, "Test Name");
+
+        //Assert
+        Assert.Equal(entityId, testEntity.Id);
+    }
+
+    [Fact]
+    public void BaseEntity_SetName_SuccessfulSet()
+    {
+        //Arrange
+        var entityId = Guid.NewGuid();
+        var testEntity = new TestEntity(entityId, "Test Name");
+
+        //Act
+        ((IEntity)testEntity).Name = "New Name";
+
+        //Assert
+        Assert.Equal("New Name", ((IEntity)testEntity).Name);
+    }
+}
---
+++ Logger/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Logger;
+
+public abstract class BaseEntity : IEntity
+{
+    public Guid Id { get; init; }
+
+    //Implemented explicitly to avoid naming contradictions
+    string IEntity.Name
+    {
+        get
+        {
+            return GetName();
+        }
+
+        set
+        {
+            SetName(value);
+        }
+    }
+
+    protected abstract string GetName();
+    protected abstract void SetName(string name);
+}
