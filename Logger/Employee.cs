93070ea5ae164125763b9c56c900cbc0d19acb1d
Nathan <nmarsee@ewu.edu>
2024-02-03 22:39:45 -0800

    Possible Employee/Student implementation
    Feedback requested

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -1,15 +1,38 @@
-﻿namespace Logger
+﻿using System.Text;
+
+namespace Logger
 {
+    //thoughts on this implementation of Employee/Student?
     public record Employee : BaseEntity
     {
+        private FullName fullName = new("", null, "");
         protected override string GetName()
         {
-            throw new NotImplementedException();
+            StringBuilder result = new();
+            result.Append(fullName.FirstName);
+            if(fullName.MiddleName is not null)
+            {
+                result.Append(" " + fullName.MiddleName);
+            }
+            result.Append(" " + fullName.LastName);
+            return result.ToString();
         }
 
         protected override void SetName(string name)
         {
-            throw new NotImplementedException();
+            string[] names = name.Split();
+            if (names.Length == 2)
+            {
+                fullName = new(names[0], names[1]);
+            }
+            else if (names.Length == 3)
+            {
+                fullName = new(names[0], names[1], names[2]);
+            }
+            else
+            {
+                throw new ArgumentException("invalid name");
+            }
         }
     }
 }
