7894b13a00b92228444ffa221b194f16724c0e1a
Nathan <nmarsee@ewu.edu>
2024-02-03 22:38:24 -0800

    Redo FullName to require first and last name to not be null

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -2,14 +2,20 @@
 
 public record FullName
 {
-    public string? FirstName { get; }
+    public string FirstName { get; }
     public string? MiddleName { get; }
-    public string? LastName { get; }
+    public string LastName { get; }
 
-    public FullName(string? firstName, string? middleName, string? lastName)
+    public FullName(string firstName, string? middleName, string lastName)
     {
-        FirstName = firstName ?? "";
+        FirstName = firstName;
         MiddleName = middleName ?? "";
-        LastName = lastName ?? "";
+        LastName = lastName;
+    }
+    public FullName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        MiddleName = null;
+        LastName = lastName;
     }
 }
