1f8125b63a9e915256e95840e36d6bd473c7111e
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-07 14:34:06 -0800

    Fix namespaces

--- Logger.Tests/BookTests.cs
+++ Logger.Tests/BookTests.cs
@@ -1,24 +1,23 @@
 ﻿using Xunit;
 
-namespace Logger.Tests
+namespace Logger.Tests;
+
+public class BookTests
 {
-    public class BookTests
+    [Fact]
+    public void Book_WithFullName_SuccessfullyCreated()
     {
-        [Fact]
-        public void Book_WithFullName_SuccessfullyCreated()
-        {
-            // Arrange
-            string name = "Test Book";
-            var author = new FullName("First", "Last");
-            int isbn = 1234567890;
+        // Arrange
+        string name = "Test Book";
+        var author = new FullName("First", "Last");
+        int isbn = 1234567890;
 
-            // Act
-            var book = new Book(name, author, isbn);
+        // Act
+        var book = new Book(name, author, isbn);
 
-            // Assert
-            Assert.Equal(name, book.Name);
-            Assert.Equal(author.ToString(), book.Author);
-            Assert.Equal(isbn, book.Isbn);
-        }
+        // Assert
+        Assert.Equal(name, book.Name);
+        Assert.Equal(author.ToString(), book.Author);
+        Assert.Equal(isbn, book.Isbn);
     }
 }
