fb705fd3c42729a458617775e15d4d14e277afe8
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-07 15:25:43 -0800

    More tests for Student Class

---
+++ Logger.Tests/StudentTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Logger.Tests;
+
+public class StudentTests
+{
+    [Fact]
+    public void Student_GetStudentId_SuccessfulGet()
+    {
+        //Arrange
+        var student = new Student("Test Student", 1234567890);
+
+        //Assert
+        Assert.Equal(1234567890, student.Sid);
+    }
+
+    [Fact]
+    public void Student_GetName_SuccessfulGet()
+    {
+        //Arrange
+        var student = new Student("Test Student", 1234567890);
+
+        //Assert
+        Assert.Equal("Test Student", student.Name);
+    }
+}
