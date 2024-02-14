239d891d128c73818d54c53e1364df5f70de0d2e
Nathan <nmarsee@ewu.edu>
2024-02-13 21:45:06 -0800

    Fix null issues

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -3,61 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace GenericsHomework
+namespace GenericsHomework;
+public class Node<T>
 {
-    public class Node<T>
+    //Type T so that values are homogenous
+    public T Value { get; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
     {
-        //Type T so that values are homogenous
-        public T Value { get; }
-        public Node<T> Next { get; private set; }
+        Value = value;
 
-        public Node(T value)
-        {
-            Value = value;
-
-            // Points at itself initially
-            Next = this;
-        }
-
-        public override string ToString()
-        {
-            return Value.ToString();
-        }
-
-
-        public void Append(T value)
-        {
-            if (Exists(value))
-            {
-                throw new InvalidOperationException("Error: No duplicate values allowed");
-            }
-
-            Node<T> newNode = new Node<T>(value);
-            newNode.Next = Next;
-            Next = newNode;
-        }
-
-        public void Clear()
-        {
-            //Since the list is singly linked, they will be available for garbage collection
-            Next = this;
-        }
-
-        public bool Exists(T value)
-        {
-            Node<T> current = this;
-
-            do
-            {
-                if (current.Value.Equals(value)) return true;
-                current = current.Next;
-
-            } while (current != this);
-            return false;
-        }
-
-
-
+        // Points at itself initially
+        Next = this;
     }
-}
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? throw new InvalidOperationException($"{nameof(Value)} is null.");
+    }
+
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+        {
+            throw new InvalidOperationException("Error: No duplicate values allowed");
+        }
+
+        Node<T> newNode = new Node<T>(value);
+        newNode.Next = Next;
+        Next = newNode;
+    }
+
+    public void Clear()
+    {
+        //Since the list is singly linked, they will be available for garbage collection
+        Next = this;
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> current = this;
+
+        do
+        {
+            bool isFound = current.Value?.Equals(value) ?? throw new InvalidOperationException(nameof(Value));
+            if (isFound) 
+                return true;
+            current = current.Next;
+
+        } while (current != this);
+        return false;
+    }
+    }
