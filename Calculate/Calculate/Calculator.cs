b8f02c676b7d8bf382cc44a33177a626c49a0e5c
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-18 23:55:30 -0800

    Calculator Class created, static methods created for math operations

---
+++ Calculate/Calculate/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculate;
+
+public class Calculator
+{
+    public static double Add(double a, double b) => a + b;
+    public static double Subtract(double a, double b) => a - b;
+    public static double Multiply(double a, double b) => a * b;
+    public static double Divide(double a, double b) 
+    { 
+        if (b == 0)
+        {
+            throw new ArgumentException("Can't divide by 0");
+        }
+
+        return a / b;
+            
+    }
+}
