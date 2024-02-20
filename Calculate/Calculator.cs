d06e102d5a3aee77b4218fc75d9ad16a3ec468cf
Nathan <nmarsee@ewu.edu>
2024-02-19 17:24:05 -0800

    Implement TryCalculate, change inputs to ints

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -5,10 +5,10 @@ namespace Calculate;
 
 public class Calculator
 {
-    public static double Add(double a, double b) => a + b;
-    public static double Subtract(double a, double b) => a - b;
-    public static double Multiply(double a, double b) => a * b;
-    public static double Divide(double a, double b) 
+    public static double Add(int a, int b) => a + b;
+    public static double Subtract(int a, int b) => a - b;
+    public static double Multiply(int a, int b) => a * b;
+    public static double Divide(int a, int b) 
     { 
         if (b == 0)
         {
@@ -19,12 +19,45 @@ public class Calculator
             
     }
 
-    public static IReadOnlyDictionary<char, Func<double, double, double>> MathematicalOperations { get; }
-        = new Dictionary<char, Func<double, double, double>>
+    public static IReadOnlyDictionary<char, Func<int, int, double>> MathematicalOperations { get; }
+        = new Dictionary<char, Func<int, int, double>>
         {
             {'+', Add },
             {'-', Subtract },
             {'*', Multiply },
             {'/', Divide },
         };
+
+    public static bool TryCalculate(string input, out double output)
+    {
+        string[] items = input.Split();
+        if (items.Length != 3 )
+        {
+            output = 0;
+            return false;
+        }
+        int first;
+        bool firstValid = int.TryParse(items[0], out first);
+        int second;
+        bool secondValid = int.TryParse(items[2], out second);
+        if (!(firstValid && secondValid))
+        {
+            output = 0;
+            return false;
+        }
+        if(items[1].Length > 1)
+        {
+            output = 0;
+            return false;
+        }
+        char operationChar = items[1].ToCharArray()[0];
+        if(operationChar != '+' && operationChar != '-' && operationChar != '*' && operationChar != '/')
+        {
+            output = 0;
+            return false;
+        }
+        output = MathematicalOperations[operationChar](first, second);
+
+        return true;
+    }
 }
--- CalculateTests/CalculatorTests.cs
+++ CalculateTests/CalculatorTests.cs
@@ -9,7 +9,7 @@ public class CalculatorTests
     [Theory]
     [InlineData(1,2,3)]
     [InlineData(2,-3,-1)]
-    public void Add_TwoDoubles_SuccessfulAdd(double a, double b, double result)
+    public void Add_TwoDoubles_SuccessfulAdd(int a, int b, double result)
     {
         // Act
         double number = Calculator.Add(a, b);
@@ -24,7 +24,7 @@ public class CalculatorTests
     [InlineData('-', 2,3,-1)]
     [InlineData('*', 2,3,6)]
     [InlineData('/', 6,3,2)]
-    public void MathematicalOperations_UsingGenericDictionary_SuccessfulAdd(char operationSymbol, double a, double b, double result)
+    public void MathematicalOperations_UsingGenericDictionary_SuccessfulAdd(char operationSymbol, int a, int b, double result)
     {
         //Arrange
         var operation = Calculator.MathematicalOperations[operationSymbol];
@@ -37,9 +37,44 @@ public class CalculatorTests
     }
 
     [Theory]
+    [InlineData("1 + 2", 3)]
+    [InlineData("2 - 3", -1)]
+    [InlineData("2 * 3", 6)]
+    [InlineData("6 / 3", 2)]
+    public void TryCalculate_ValidInputs_SuccessfulOperation(string input, double result)
+    {
+        //Arrange
+        double output = 0;
+
+        //Act
+        bool canCalculate = Calculator.TryCalculate(input, out output);
+
+        //Assert
+        Assert.True(canCalculate);
+        Assert.Equal(result, output);
+    }
+
+    [Theory]
+    [InlineData("1 2")]
+    [InlineData("1 2 3")]
+    [InlineData("1 + 2 3")]
+    [InlineData("a * 3")]
+    public void TryCalculate_InvalidInputs_UnsuccessfulOperation(string input)
+    {
+        //Arrange
+        double output = 0;
+
+        //Act
+        bool canCalculate = Calculator.TryCalculate(input, out output);
+
+        //Assert
+        Assert.False(canCalculate);
+    }
+
+    [Theory]
     [InlineData(3, 2, 1)]
     [InlineData(2, 3, -1)]
-    public void Subtract_TwoDoubles_SuccessfulAdd(double a, double b, double result)
+    public void Subtract_TwoDoubles_SuccessfulAdd(int a, int b, double result)
     {
         // Act
         double number = Calculator.Subtract(a, b);
@@ -52,7 +87,7 @@ public class CalculatorTests
     [Theory]
     [InlineData(1, 2, 2)]
     [InlineData(2, -3, -6)]
-    public void Multiply_TwoDoubles_SuccessfulAdd(double a, double b, double result)
+    public void Multiply_TwoDoubles_SuccessfulAdd(int a, int b, double result)
     {
         // Act
         double number = Calculator.Multiply(a, b);
@@ -65,7 +100,7 @@ public class CalculatorTests
     [Theory]
     [InlineData(4, 2, 2)]
     [InlineData(2, -2, -1)]
-    public void Divide_TwoDoubles_SuccessfulAdd(double a, double b, double result)
+    public void Divide_TwoDoubles_SuccessfulAdd(int a, int b, double result)
     {
         // Act
         double number = Calculator.Divide(a, b);
@@ -76,13 +111,13 @@ public class CalculatorTests
     }
     
     [Fact]
-public void Divide_ByZeroThrowsArgumentException()
-{
-    // Arrange
-    double a = 5;
-    double b = 0;
+    public void Divide_ByZeroThrowsArgumentException()
+    {
+        // Arrange
+        int a = 5;
+        int b = 0;
 
-    // Act & Assert
-    Assert.Throws<ArgumentException>(() => Calculator.Divide(a, b));
-}
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Calculator.Divide(a, b));
+    }
 }
