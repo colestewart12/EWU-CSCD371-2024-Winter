79c0f152f37fd9bd652dadd6ea9c2a06452f328a
colestewart12 <120848582+colestewart12@users.noreply.github.com>
2024-02-21 09:26:41 -0800

    Empty Default Constructor

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -4,14 +4,12 @@ using System;
 public class Program
 {
     //Action is for referring to void-returning methods
-    public Action<string> WriteLine { get; init; }
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
     //Func is for referring to methods that return a value
-    public Func<string?> ReadLine { get; init; }
+    public Func<string?> ReadLine { get; init; } = Console.ReadLine;
 
     public Program()
     {
-        WriteLine = Console.WriteLine;
-        ReadLine = Console.ReadLine;
     }
     static void Main(string[] args)
     {
