2ddfc42f3b41efe419ce81f530535a1bd2e8d2c4
Cole Stewart <colesoccerstewy@gmail.com>
2024-02-29 12:35:15 -0800

    Test for initial csv rows

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment.Tests;
+
+public class SampleDataTests
+{
+    [TestMethod]
+    public void CsvRows_ShouldSkipHeaderAndReturnCorrectLines()
+    {
+        // Arrange
+        var sampleData = new SampleData();
+
+        // Act
+        var csvRows = sampleData.CsvRows;
+        var csvRowsCount = sampleData.CsvRows.Count;
+
+        // Assert
+        Assert.Equals(49, csvRowsCount);
+    }
+}
